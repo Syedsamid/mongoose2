aa73994a72a0c3dbe63851a9d6f1925f7dad9331
Syedsamid <samidsyed1720@gmail.com>
2024-12-19 11:19:13 +0000

    data added

--- Backend/controllers/users/index.js
+++ Backend/controllers/users/index.js
@@ -1,33 +1,85 @@
 import express from "express"
 import userModel from "../../models/Users/index.js";
+import { error } from "console";
 const router = express.Router();
 
-router.get("/getallusers",(req,res)=>{
+// get all api
+
+router.get("/getallusers",async (req,res)=>{
     try {
-        console.log("Get all the users");
-        res.status(200).json({msg:"hello",
-            
-                "firstname": "syed samid",
-                "lastname": "Suhail Roushan",
-                "email": "samid123@gmail.com",
-                "age": 23,
-                "phone": "+91 80109394098"
-            })
+
+        console.log("i came here");
+
+      let allData = await userModel.find({});
+      console.log(allData);
+
+      res.status(200).json(allData)  
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg:error})
+    }
+})
+
+router.get("/getoneuser",async(req,res)=>{
+    try {
+        let check = await userModel.find({ email: "suhaiel@code.in" });
+        console.log(check);
+        res.status(200).json(check)
     } catch (error) {
         console.log(error);
         res.status(500).json({msg:error})
     }
 })
 
-router.get(".getaall",async(req,res)=>{
-    try{
-        let users = await userModel.find({firstname:"Ali"});
-        res.status(200).json(users);
 
-    }catch(error){
-        console.log(error)
+router.post("/add",async(req,res)=>{
+    try {
+        let userInput = req.body
+
+        console.log(userInput)
+        await userModel.create(userInput)
+        res.status(200).json(userInput)
+    } catch (error) {
+        console.log(error);
         res.status(500).json({msg:error})
     }
+})
+
+
+router.put("/updateuser",async(req,res)=>{
+    try {
+        let check = await userModel.updateOne(
+            { email: "suhail@code.in" },
+            { $set: { firstname: "aliss" } }
+          );
+          res.status(200).json({msg:"data updated"})
+          console.log(check)
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg:error})
+    }
+})
+
+
+router.delete("/userdeleteOne",(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg:error})
+    }
+})
+
+router.delete("/userdeleteAll",(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg:error})
+    }
+
 })
 
 export default router
